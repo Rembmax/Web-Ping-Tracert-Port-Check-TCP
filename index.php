3b2e4d03fee2e43344becc6a6fc5ebe61b61d4ea
Rembmax <max.remb@gmail.com>
2020-09-23 15:24:44 -0300

    Update check

--- index.php
+++ index.php
@@ -1,59 +1,61 @@
 <!DOCTYPE html>
-	<?php	include ('head.php');	?>
-  <?php	include ('function.php');	?>
-
-  <html lang="en">
+<?php	include ('head.php');	?>
+<?php	include ('function.php');	?>
+<html lang="en">
 <body>
 <div class="jumbotron text-center" style="margin-bottom:0">
-  <h1>Php Option Network</h1>
-  <p>Option Network: Ping / Nmap / ....</p>
+<h1>Php Option Network</h1>
+<br>
+<h5> Ping / Nmap / Tracert</h5>
 </div>
-
 <br>
 <br>
 <br>
 <br>
-
 <div class="container">
-
-<div class="row">
-    <div class="col-sm-2"></div>
-    <div class="col-sm-4 text-center">
-
-
-</div>
-<div class="col-sm-4 text-center">
-
-
-</div>
-
-
-
-<div class="col-sm-2"></div>
-</div>
-
-
-
-
 <div class="row">
 <div class="col-sm-2"></div>
-    <div class="col-sm-8">
-			<h5 class="text-primary text-center"> You IP: <?PHP echo getUserIP(); ?> </h5>
-			<button type="button" id="ready" class="btn btn-success btn-block">Ready ?</button>
-
-			<?PHP
-			/*
-			$result = shell_exec('ping 8.8.8.8 -c 5');
-			echo $result;
-			 */
-			?>
-
-
-
-
+<div class="col-sm-8 text-center">
+<h5> Your IP is: <?php echo getUserIP();?></h5><br>
+<form class="" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+<input class="form-control mr-sm-2 text-center" name="IpCheck" type="text" placeholder="Enter Destination Ip " minlength="7" ma
+xlength="15" size="15" pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$"><br>
+<div class="text-info">
+<label for="sel1">Protocol</label>
+<select class="form-control text-center" id="sel1" name="protocol">
+<option value="none" selected>----</option>
+<option value="Ping">Ping</option>
+<option value="Nmap">Nmap</option>
+<option value"Tracert">Tracert</option>
+</select>
 </div>
-
-<div class="col-sm-2"></div>
+<br>
+<div class="form-group">
+<button class="btn btn-success"  type="submit">Submit</button>
+</div>
+</form>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$IP = $_POST['IpCheck'];
+$Protocol = $_POST['protocol'];
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+if(!empty($IP) and $Protocol != '----'){
+switch ($Protocol) {
+case 'Ping':
+        echo "<br><br><pre>".Ping($IP)."</pre>";
+        break;
+    case 'Nmap':
+        echo "<br><br><pre>".Nmap($IP)."</pre>";
+        break;
+    case 'Tracert':
+        echo "<br><br><pre>".Tracert($IP)."</pre>";
+        break;
+}// END switch ($Protocol)
+}// END if(!empty($IP) and $Protocol != '----')
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+}// END ($_SERVER["REQUEST_METHOD"] == "POST")
+?>
+</div>
 </div>
 </body>
-  </html>
+</html>
